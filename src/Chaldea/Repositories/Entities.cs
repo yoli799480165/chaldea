1811ae0d348b94b0336346c3c6c3387a80b2d93b
zhaowenkai <799480165@qq.com>
2018-10-11 13:10:19 +0800

    update entities

--- src/Chaldea/Repositories/Entities.cs
+++ src/Chaldea/Repositories/Entities.cs
@@ -4,18 +4,10 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace Chaldea.Repositories
 {
-    public class Bangumi : IEntity<string>
+    public class Anime
     {
-        public string Name { get; set; }
-        public List<Anime> Animes { get; set; }
+        public string AnimeId { get; set; }
 
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-    }
-
-    public class Anime : IEntity<string>
-    {
         public string Title { get; set; }
 
         public string Cover { get; set; }
@@ -23,15 +15,6 @@ namespace Chaldea.Repositories
         public long PlayCounts { get; set; }
 
         public long SubCounts { get; set; }
-
-        [BsonId]
-        [BsonRepresentation(BsonType.ObjectId)]
-        public string Id { get; set; }
-    }
-
-    public class AnimeDetail : IEntity<string>
-    {
-        public string AnimeId { get; set; }
 
         public string Desc { get; set; }
 
@@ -45,44 +28,70 @@ namespace Chaldea.Repositories
 
         public string Type { get; set; }
 
+        public int Level { get; set; }
+
         public List<string> Tags { get; set; }
 
-        public List<Resource> Animes { get; set; }
+        public List<string> Videos { get; set; }
 
-        public List<Resource> Comics { get; set; }
+        public List<string> Comics { get; set; }
 
-        public List<Resource> Novels { get; set; }
+        public List<string> Novels { get; set; }
 
-        public List<Comment> Comments { get; set; }
+        public List<string> Comments { get; set; }
+    }
+
+    public class Bangumi : IEntity<string>
+    {
+        public string Name { get; set; }
+
+        public List<Anime> Animes { get; set; }
 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
     }
 
-    public class Resource
+    public class Video : IEntity<string>
     {
-        public string Uid { get; set; }
-
         public string Name { get; set; }
 
         public string Url { get; set; }
 
-        public MediaMetaData MetaData { get; set; }
+        public int Duration { get; set; }
+
+        public int FrameWidth { get; set; }
+
+        public int FrameHeight { get; set; }
+
+        public int FrameRate { get; set; }
+
+        public long Length { get; set; }
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
     }
 
-    public class MediaMetaData
+    public class Comic : IEntity<string>
     {
-        public int Duration { get; set; }
-        public int FrameWidth { get; set; }
-        public int FrameHeight { get; set; }
-        public int FrameRate { get; set; }
-        public long Length { get; set; }
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+
+    public class Novel : IEntity<string>
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
     }
 
     public class Comment : IEntity<string>
     {
         public string Content { get; set; }
+
+        public string UserId { get; set; }
 
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
@@ -92,7 +101,9 @@ namespace Chaldea.Repositories
     public class Banner : IEntity<string>
     {
         public string Image { get; set; }
+
         public string Link { get; set; }
+
         public string Title { get; set; }
 
         [BsonId]
